f90ac2840328b121398964833cdb94017c949853
Andreas Kugel <andreas.kugel@web.de>
2022-04-29 14:40:27 +0200

    add spinner

--- docs/.vuepress/components/Loading.vue
+++ docs/.vuepress/components/Loading.vue
@@ -1,21 +1,29 @@
 <template>
 
   <CCallout color="info">
+
+    <CSpinner/>
+
     {{ message }}
+
+
   </CCallout>
+
 
 </template>
 
 <script>
 import { CCallout } from '@coreui/vue'
+import { CSpinner } from '@coreui/vue'
 
 import '../public/css/colors.css'
 import '../public/css/callout.css'
+import '../public/css/spinner.css'
 
 export default {
     props: ["message"],
     components: {
-    CCallout,
+    CCallout,CSpinner
     },
 }
 
