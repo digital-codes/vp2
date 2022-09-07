5abb34f7c309deb33f9c1e5e20fe6a7ae58ea669
Andreas Kugel <andreas.kugel@web.de>
2022-09-07 21:48:18 +0200

    404 page improved

--- docs/.vuepress/theme/layouts/NotFound.vue
+++ docs/.vuepress/theme/layouts/NotFound.vue
@@ -2,8 +2,6 @@
 import { useRouteLocale } from '@vuepress/client'
 import { useThemeLocaleData } from '@vuepress/theme-default/client'
 
-import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
-
 const routeLocale = useRouteLocale()
 
 const themeLocale  = useThemeLocaleData()
@@ -14,27 +12,41 @@ const messages = themeLocale.value.notFound ?? ['Not Found']
 const getMsg = (): string =>
   messages[Math.floor(Math.random() * messages.length)]
 
-
-// ------ test ---------
-import { useThemeData } from '@vuepress/theme-default/client'
-const themeData  = useThemeData()
-console.log("TD:",themeData.value)
-console.log("TL:",themeLocale.value)
+import Navbar from '@theme/Navbar.vue'
 
 
 </script>
 
 <template>
-  <ParentLayout>
-    <template #page-content-top>
+  <div
+    class="theme-container"
+  >
+    <slot name="navbar">
+      <Navbar>
+        <template #before>
+          <slot name="navbar-before" />
+        </template>
+        <template #after>
+          <slot name="navbar-after" />
+        </template>
+      </Navbar>
+    </slot>
+
+  <div class="theme-container">
+    <main class="page">
+      <div class="theme-default-content">
         <h1>404</h1>
+
         <blockquote>{{ getMsg() }}</blockquote>
+
         <RouterLink :to="homeLink">{{ homeText }}</RouterLink>
-    </template>
-    <template #page-bottom>
-      <div class="my-footer">This is my custom page footer</div>
-    </template>
-  </ParentLayout>
+      </div>
+    </main>
+  </div>
+
+  <div class="my-footer">This is my custom page footer</div>
+
+</div>
 </template>
 
 
@@ -58,48 +70,3 @@ console.log("TL:",themeLocale.value)
 </style>
 
 
-<style>
-
-/*
-vue adds a default message if no page content available.
-Add class content404 in 
-
-// src/components/Content.ts
-import { pagesComponents } from "@internal/pagesComponents";
-import { computed, defineComponent as defineComponent2, h } from "vue";
-var Content = defineComponent2({
-  name: "Content",
-  props: {
-    pageKey: {
-      type: String,
-      required: false,
-      default: ""
-    }
-  },
-  setup(props) {
-    const page = usePageData();
-    const pageComponent = computed(
-      () => pagesComponents[props.pageKey || page.value.key]
-    );
-    return () => pageComponent.value ? h(pageComponent.value) : h(
-      "div",
-      // AKU fixme
-      { class: 'content404', 
-      innerHTML: __VUEPRESS_DEV__ ? "Page does not exist. This is a fallback content." : "404 Not Found"
-      }
-    );
-  }
-});
-
-or in the distribution file, like 
-node_modules/@vuepress/client/dist/chunk-O5XLV5GU.js
-
-*/
-
-
-.content404 {
-  display:none;
-}
-
-
-</style>
---
+++ docs/hidden.md
@@ -0,0 +1,26 @@
+---
+title: Hidden in Navigation
+description: Teaser für Beschreibung
+head:
+  - - meta
+    - name: foo
+      content: yaml array syntax
+  - [meta, { name: bar , content: square brackets syntax }]
+
+
+---
+
+# Hidden
+
+sdqdef
+
+kdqlknqekf+
+qw
+fd
++qefwefwefwefrwfre
+
+## More 
+
+
+fwefwef
+
